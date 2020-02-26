29f75d7740ecac7eaccba1add1b89f5985ec33b8
unknown <iapdoo>
2020-02-26 10:50:28 +0200

    first commet

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -43,45 +43,27 @@
                         </h3>
                     </div>
                     <div class="panel-body">
-                    {!! Form::open(['action' => ['UsersController@edit',$user->id],'method' => 'PATCH']) !!}
-<div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Update</div>
-                    <div class="panel-body" style="height: 670px;">
-
+{!! Form::model($user,['method' => 'PATCH','action'=>['UsersController@edit','id'=>$user->id]]) !!}
     <div class="form-group">
-        {{Form::label('name', 'Name')}}
+        {{Form::label('name', 'الاسم ')}}
         {{Form::text('name', $user->name,['class' => 'form-control','placeholder'=>'Name'])}}
     </div>
     <div class="form-group">
-        {{Form::label('email', 'Email')}}
+        {{Form::label('email', 'البريد الالكتروني')}}
         {{Form::text('email', $user->email ,['class' => 'form-control','placeholder'=>'Email'])}}
     </div>
      
     <div class="form-group">
-            {{Form::label('admin', 'User Type')}}
-            {!!Form::select('role',['patient'=>'Patient','doctor'=>'Doctor','admin'=>'Admin'],$user->role,['class'=>'form-control']) !!}
-        </div>   
-    {{Form::submit('Submit',['class'=>'btn btn-primary'])}}
+            {{Form::label('admin', 'التحكم')}}
+            {!!Form::select('admin' , ['0' => 'user', '1' => 'admin'], null ,['class'=>'form-control']) !!}
+        </div>
+
+        
 
          
 
-                    </div>
-                </div>
-            </div>
-        </div>
-</div>
-
-
+    {{Form::submit('حفظ التعديل ',['class'=>'btn btn-primary'])}}
 {!! Form::close() !!}
-
-
-
-
-
-
                     </div>
                 </div>
             </div>
